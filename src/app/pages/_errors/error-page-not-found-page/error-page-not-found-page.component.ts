491becdb0fdec6cf9b8a072598e79146e30761f1
Lassi <csharplassi@posteo.de>
2023-03-02 12:58:33 +0100

    Add: OnInit in ErrorPageNotFoundPageComponent

--- src/app/pages/_errors/error-page-not-found-page/error-page-not-found-page.component.ts
+++ src/app/pages/_errors/error-page-not-found-page/error-page-not-found-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Optional, PLATFORM_ID} from '@angular/core';
+import {Component, Inject, OnInit, Optional, PLATFORM_ID} from '@angular/core';
 import {REQUEST, RESPONSE} from "@nguniversal/express-engine/tokens";
 import {isPlatformServer} from "@angular/common";
 import {Request, Response} from 'express';
@@ -8,7 +8,7 @@ import {Request, Response} from 'express';
   templateUrl: './error-page-not-found-page.component.html',
   styleUrls: ['./error-page-not-found-page.component.scss']
 })
-export class ErrorPageNotFoundPageComponent {
+export class ErrorPageNotFoundPageComponent implements OnInit {
   constructor(@Optional() @Inject(REQUEST) private request: Request,
               @Optional() @Inject(RESPONSE) private response: Response,
               @Inject(PLATFORM_ID) private platformId: any) {
